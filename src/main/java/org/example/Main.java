d3970ce09beafef757363da5de3d3c53353eba62
GOLDER303 <szymonstepien@interia.pl>
2025-05-11 13:47:24 +0200

    feat: create window manager class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,16 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        WindowManager window = new WindowManager("Market Agent Simulation", 1600, 900, false);
+        window.init();
+
+        System.out.println("Window initialized successfully");
+
+        while (!window.windowShouldClose()) {
+            window.update();
+        }
+
+        window.cleanup();
     }
 }
