31102127c4c9f2b591f59221a9738d60e3ca2d9c
GOLDER303 <szymonstepien@interia.pl>
2025-05-21 16:30:51 +0200

    feat: integrate sight sensor in customer movement

--- Assets/Scripts/CustomerMovement.cs
+++ Assets/Scripts/CustomerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Customer : MonoBehaviour
@@ -8,18 +9,40 @@ public class Customer : MonoBehaviour
     [SerializeField]
     private float speed = 5f;
 
+    [SerializeField]
+    private List<string> productList = new();
+
+    private HashSet<string> productSet;
     private Vector3 currentTargetPoint;
     private int currentTargetPointIndex = 0;
     private Rigidbody rb;
+    private SightSensor sightSensor;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        sightSensor = GetComponentInChildren<SightSensor>();
+
+        productSet = new HashSet<string>(productList);
 
         currentTargetPoint = path.GetControlPoint(0);
     }
 
     void Update()
+    {
+        Product product = GetProductFromListInSight();
+
+        if (product != null)
+        {
+            MoveTowardsPosition(product.transform.position);
+        }
+        else
+        {
+            MoveAlongWaypointPath();
+        }
+    }
+
+    private void MoveAlongWaypointPath()
     {
         Vector2 targetXZ = new(currentTargetPoint.x, currentTargetPoint.z);
         Vector2 positionXZ = new(transform.position.x, transform.position.z);
@@ -31,12 +54,32 @@ public class Customer : MonoBehaviour
             currentTargetPoint = path.GetControlPoint(++currentTargetPointIndex);
         }
 
-        Vector3 direction = currentTargetPoint - transform.position;
+        MoveTowardsPosition(currentTargetPoint);
+    }
+
+    private void MoveTowardsPosition(Vector3 targetPosition)
+    {
+        Vector3 direction = targetPosition - transform.position;
         direction.y = 0;
 
         Quaternion lookRotation = Quaternion.LookRotation(direction);
         transform.rotation = lookRotation;
 
         rb.linearVelocity = direction.normalized * speed;
+    }
+
+    private Product GetProductFromListInSight()
+    {
+        foreach (GameObject objectInSight in sightSensor.ObjectsInSight)
+        {
+            Product product = objectInSight.GetComponent<Product>();
+
+            if (product != null && productSet.Contains(product.ProductSO.name))
+            {
+                return product;
+            }
+        }
+
+        return null;
     }
 }
