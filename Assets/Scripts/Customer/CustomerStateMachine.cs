3e21f96134355381aaa5a274e0be8def5518c43d
GOLDER303 <szymonstepien@interia.pl>
2025-05-27 16:49:13 +0200

    fix: fix moving to checkout state in customer state machine

--- Assets/Scripts/Customer/CustomerStateMachine.cs
+++ Assets/Scripts/Customer/CustomerStateMachine.cs
@@ -26,7 +26,7 @@ public class CustomerStateMachine : StateMachine<CustomerStateMachine.CustomerSt
         states.Add(CustomerState.FollowingPath, new FollowingPathState(this, customer, path));
         states.Add(CustomerState.MovingToProduct, new MovingToProductState(this, customer));
         states.Add(CustomerState.PickingUpProduct, new PickingUpProductState(this, customer));
-        states.Add(CustomerState.MovingToCheckout, new MovingToProductState(this, customer));
+        states.Add(CustomerState.MovingToCheckout, new MovingToCheckoutState(this, customer));
 
         currentState = states[CustomerState.FollowingPath];
     }
